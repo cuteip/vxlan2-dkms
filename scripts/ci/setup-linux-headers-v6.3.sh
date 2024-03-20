2d4df989d7c08c82f2ff7313bec6db41876fed53
sobadon <sbdn@chasoba.net>
2024-03-20 22:15:26 +0900

    ci v6.3

---
+++ scripts/ci/setup-linux-headers-v6.3.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -ex
+
+wget --quiet --no-clobber https://kernel.ubuntu.com/mainline/v6.3/amd64/linux-headers-6.3.0-060300_6.3.0-060300.202304232030_all.deb
+wget --quiet --no-clobber https://kernel.ubuntu.com/mainline/v6.3/amd64/linux-headers-6.3.0-060300-generic_6.3.0-060300.202304232030_amd64.deb
+
+dpkg -i ./*.deb
+rm *.deb
