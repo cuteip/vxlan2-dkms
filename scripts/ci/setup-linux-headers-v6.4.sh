417c4d4c89835a79ca93532574e2af08fa823522
sobadon <sbdn@chasoba.net>
2024-03-20 22:09:28 +0900

    ci v6.4

---
+++ scripts/ci/setup-linux-headers-v6.4.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -ex
+
+wget --quiet --no-clobber https://kernel.ubuntu.com/mainline/v6.4/amd64/linux-headers-6.4.0-060400_6.4.0-060400.202306271339_all.deb
+wget --quiet --no-clobber https://kernel.ubuntu.com/mainline/v6.4/amd64/linux-headers-6.4.0-060400-generic_6.4.0-060400.202306271339_amd64.deb
+
+dpkg -i ./*.deb
+rm *.deb
