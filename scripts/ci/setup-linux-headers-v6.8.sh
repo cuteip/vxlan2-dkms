f996fcc3faa1f31b81235448646efe936c14602b
sobadon <sbdn@chasoba.net>
2025-02-22 02:53:29 +0900

    add v6.8 ci script

---
+++ scripts/ci/setup-linux-headers-v6.8.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -ex
+
+wget --quiet --no-clobber https://file-public1.chasoba.net/mirrors/kernel.ubuntu.com/mainline/v6.7/amd64/linux-headers-6.7.0-060700_6.7.0-060700.202401072033_all.deb
+wget --quiet --no-clobber https://file-public1.chasoba.net/mirrors/kernel.ubuntu.com/mainline/v6.7/amd64/linux-headers-6.7.0-060700-generic_6.7.0-060700.202401072033_amd64.deb
+
+dpkg -i ./*.deb
+rm *.deb
